5478b40d2a4e635a2b025b0e21cce30ebe5181c9
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2024-01-17 18:13:10 +0100

    Theoretical emoji support

--- config.mk
+++ config.mk
@@ -7,8 +7,10 @@ VERSION = 0.8.5
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
+X11INC = /home/jan/Software/libxft/include
+X11LIB = /home/jan/Software/libxft/src/.libs
 
 PKG_CONFIG = pkg-config
 
